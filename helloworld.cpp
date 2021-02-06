e40e8587ec4054639ca196df9ec2f83a903884ed
LAPTOP-QBUK4S20\Lydia Azra <lydia.azra@gmail.com>
2021-02-06 13:51:33 +0800

    tengokkkk ni

--- helloworld.cpp
+++ helloworld.cpp
@@ -7,5 +7,7 @@ int main()
 	cout<<"Hello dunia tipu-tipu"<<endl;
         cout<<"boleh tenogk coding saya"<<endl;
         	cout<<"sambungan 2:15 ...."<<endl;
+            cout<<"testtt"<<endl;
+        	cout<<"kenapa benda ni melecehkan"<<endl;
 
 }
